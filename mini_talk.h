95857e7e8a643fb6ef63311df05e3f166a71d015
Rosa Fernandes <rosferna@c2r10s7.42lisboa.com>
2022-10-26 13:20:39 +0100

    decode

--- mini_talk.h
+++ mini_talk.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
 void client(pid_t pid, unsigned char *str);
 void    encode(int i, pid_t pid);
 
-#endif
+ #endif
--- server.c
+++ server.c
@@ -3,20 +3,20 @@
 int main()
 {
     char *str = "01110010";
+    char *tmp = NULL;
     int z = 0;
 
     int i = 0;
-    while (i > 8)
+    while (i < 8)
     {
         if (str[i] == '1')
         {
             z = (z * 2) + 1;
-            printf("%d ", z);
         }
         else 
             z = (z * 2) + 0;
-        i++;
+        i++;   
     }
-    //char c = (char)z;
-    printf("%i\n", z);
+    char x = z;
+    printf("%c", x);
 }
